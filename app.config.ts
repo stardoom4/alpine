90e4bfa0f431a1b8fe66b6662e00681062eef2ab
stardoom4 <139852159+stardoom4@users.noreply.github.com>
2023-12-06 23:53:19 +0530

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Celestial Mind',
+    description: 'A Personal Wiki',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
